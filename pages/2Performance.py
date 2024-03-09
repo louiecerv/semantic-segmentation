89bdb22b9c57c13c8e5d32049f1f3d4c24d38445
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-09 12:48:38 +0800

    Save

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -17,47 +17,43 @@ def app():
     X = st.session_state.X
     y = st.session_state.y
     
-    clf = KMeans(n_clusters=n_clusters)
-    clf.fit(X)
-    y_test_pred = clf.predict(X)
+    # WCSS (Within Cluster Sum of Squares) list
+    wcss_list = []
 
-    centers, labels, wcss, silhouette = find_clusters(X, n_clusters, y_test_pred)
+    # Silhouette score list
+    silhouette_scores = []
 
-    st.sidebar.write('WCSS: ' + str(wcss))
-    st.sidebar.write('Silhouette: ' + str(silhouette))
+    # Try different numbers of clusters
+    for k in range(2, 11):
+        # Create KMeans object
+        kmeans = KMeans(n_clusters=k, random_state=0)
 
-def find_clusters(X, n_clusters, y_test_pred, rseed=42):
-    #randomly choose clusters
-    rng = np.random.RandomState(rseed)
-    i = rng.permutation(X.shape[0])[:n_clusters]
-    centers = X[i]
+        # Fit the model to the data
+        kmeans.fit(X)
 
-    # Create the plot
+        # Calculate WCSS
+        wcss = kmeans.inertia_
+        wcss_list.append(wcss)
+
+        # Calculate Silhouette score
+        silhouette_score_val = silhouette_score(X, kmeans.labels_)
+        silhouette_scores.append(silhouette_score_val)
+
+    # WCSS vs Number of Clusters plot
     fig, ax = plt.subplots()
-
-    # Scatter plot for data points
-    ax.scatter(X[:, 0], X[:, 1], c=y_test_pred, s=50, cmap='CMRmap')
-
-    # Scatter plot for centers
-    ax.scatter(centers[:, 0], centers[:, 1], c='black', s=100, alpha=0.5)
-
-    # Display the plot using Streamlit
+    ax.plot(range(2, 11), wcss_list)
+    ax.set_xlabel("Number of Clusters")
+    ax.set_ylabel("WCSS")
+    ax.set_title("WCSS vs Number of Clusters")
     st.pyplot(fig)
 
-    while True:
-        labels = pairwise_distances_argmin(X, centers)
-        new_centers = np.array([X[labels==i].mean(0) for i in range(n_clusters)])
-
-        #check for convergence
-        if np.all(centers==new_centers):
-            break
-        centers = new_centers
-
-    # Calculate within-cluster sum of squared errors (WCSS)
-    wcss = np.sum([(X[labels == i] - centers[i])**2 for i in range(n_clusters)]).sum()
-    silhouette = silhouette_score(X, labels)
-
-    return centers, labels, wcss, silhouette
+    # Silhouette Score vs Number of Clusters plot
+    fig, ax = plt.subplots()
+    ax.plot(range(2, 11), silhouette_scores)
+    ax.set_xlabel("Number of Clusters")
+    ax.set_ylabel("Silhouette Score")
+    ax.set_title("Silhouette Score vs Number of Clusters")
+    st.pylot(fig)
 
 #run the app
 if __name__ == "__main__":
