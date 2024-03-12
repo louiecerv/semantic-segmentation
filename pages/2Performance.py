c442d19d169a886928e2a50983321fc50c2ea513
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-12 10:21:50 +0800

    Save

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -50,10 +50,25 @@ def app():
         
         X = st.session_state.X
         y = st.session_state.y
-                
-        # Perform DBSCAN clustering
-        db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
-        labels = db.labels_
+
+        clf = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
+
+        # Split the dataset into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, \
+            test_size=0.2, random_state=42)
+        
+        clf.fit(X_train,y_train)
+        y_test_pred = clf.predict(X_test)
+        st.subheader('Confusion Matrix')
+
+        st.write('Confusion Matrix')
+        cm = confusion_matrix(y_test, y_test_pred)
+        st.text(cm)
+        st.subheader('Performance Metrics')
+        st.text(classification_report(y_test, y_test_pred))
+        st.subheader('VIsualization')
+        visualize_classifier(clf, X_test, y_test_pred)              
+
 
         # Calculate adjusted Rand score for performance evaluation
         ari = adjusted_rand_score(y, labels)
@@ -101,6 +116,47 @@ def app():
             progress_bar.progress(i + 1)
             # Simulate some time-consuming task (e.g., sleep)
             time.sleep(0.01)
+
+def visualize_classifier(classifier, X, y, title=''):
+    # Define the minimum and maximum values for X and Y
+    # that will be used in the mesh grid
+    min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
+    min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
+
+    # Define the step size to use in plotting the mesh grid 
+    mesh_step_size = 0.01
+
+    # Define the mesh grid of X and Y values
+    x_vals, y_vals = np.meshgrid(np.arange(min_x, max_x, mesh_step_size), np.arange(min_y, max_y, mesh_step_size))
+
+    # Run the classifier on the mesh grid
+    output = classifier.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
+
+    # Reshape the output array
+    output = output.reshape(x_vals.shape)
+    
+    # Create the figure and axes objects
+    fig, ax = plt.subplots()
+
+    # Specify the title
+    ax.set_title(title)
+    
+    # Choose a color scheme for the plot
+    ax.pcolormesh(x_vals, y_vals, output, cmap=plt.cm.gray)
+    
+    # Overlay the training points on the plot
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=75, edgecolors='black', linewidth=1, cmap=plt.cm.Paired)
+    
+    # Specify the boundaries of the plot
+    ax.set_xlim(x_vals.min(), x_vals.max())
+    ax.set_ylim(y_vals.min(), y_vals.max())
+    
+    # Specify the ticks on the X and Y axes
+    ax.set_xticks(np.arange(int(X[:, 0].min() - 1), int(X[:, 0].max() + 1), 1.0))
+    ax.set_yticks(np.arange(int(X[:, 1].min() - 1), int(X[:, 1].max() + 1), 1.0))
+
+    
+    st.pyplot(fig)
 
 #run the app
 if __name__ == "__main__":
