fc83e475ff41f95b03b0dbd7e066938eb01e204e
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-15 14:23:16 +0800

    Save

--- pages/1The_Dataset.py
+++ pages/1The_Dataset.py
@@ -21,24 +21,9 @@ def app():
     \nSource: Publicly available from https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_california_housing.html
     Size: Contains 20,640 data points, each representing a block in California."""
     st.write(text)
-    # Load the California housing data
-    data = fetch_california_housing()
 
-    # Convert data features to a DataFrame
-    feature_names = data.feature_names
-    
-    df = pd.DataFrame(data.data, columns=feature_names)
-    df['target'] = data.target
-    
-    st.subheader('The California Housing Dataset')
-    st.write(df)
-
-    st.write('Data Description')
-    st.write(df.describe().T)
-
-    # Separate features and target variable
-    X = df.drop('target', axis=1)  # Target variable column name
-    y = df['target']
+    # Download data (might require internet connection)
+    X, y = fetch_openml('svhn', version=1, return_X_y=True)
 
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -48,15 +33,6 @@ def app():
     st.session_state.X_test = X_test
     st.session_state.y_train = y_train
     st.session_state.y_test = y_test
-
-    # Standardize features using StandardScaler (recommended)
-    scaler = st.session_state["scaler"] 
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    st.session_state.X_test_scaled = X_test_scaled
-
-    # save the scaler object for later use
-    st.session_state["scaler"] = scaler
 
    # Define MLP parameters    
     st.sidebar.subheader('Set the MLP Parameters')
@@ -97,52 +73,11 @@ def app():
     #store the clf object for later use
     st.session_state.clf = clf
 
-    if st.button("Show Graphs"):
-        plot_feature(df["MedInc"], df["target"], 
-                     "Median Income (Thousands USD)", 
-                     "Median House Value (Thousands USD", 
-                     "Median Income vs. Median House Value")
-    
-        plot_feature(df["HouseAge"], df["target"], 
-                     "House Age in years", 
-                     "Median House Value (Thousands USD", 
-                     "House Age vs. Median House Value")
-        
-        plot_feature(df["AveRooms"], df["target"], 
-                     "Average Rooms", 
-                     "Median House Value (Thousands USD", 
-                     "Average vs. Median House Value")
-            
-        plot_feature(df["AveBedrms"], df["target"], 
-                     "Average Bed Rooms", 
-                     "Median House Value (Thousands USD", 
-                     "Average Bed Rooms vs. Median House Value")
-        
-        plot_feature(df["Population"], df["target"], 
-                     "Population", 
-                     "Median House Value (Thousands USD", 
-                     "Population vs. Median House Value")
-        
-        plot_feature(df["AveOccup"], df["target"], 
-                     "Average Occupancy", 
-                     "Median House Value (Thousands USD", 
-                     "Average Occupancy vs. Median House Value")
-        
-        plot_feature(df["Latitude"], df["target"], 
-                     "Latitude", 
-                     "Median House Value (Thousands USD", 
-                     "Latitude vs. Median House Value")
-    
-        plot_feature(df["Longitude"], df["target"], 
-                     "Longitude", 
-                     "Median House Value (Thousands USD", 
-                     "Longitude vs. Median House Value")
-
     if st.button('Start Training'):
         progress_bar = st.progress(0, text="Training the MLP regressor can take up to five minutes please wait...")
 
         # Train the model 
-        train_model(X_train_scaled, y_train)
+        train_model(X_train, y_train)
 
         # update the progress bar
         for i in range(100):
@@ -153,19 +88,6 @@ def app():
         # Progress bar reaches 100% after the loop completes
         st.success("Regressor training completed!") 
         st.write("Use the sidebar to open the Performance page.")
-        
-def plot_feature(feature, target, labelx, labely, title):
-    # Display the plots
-    fig, ax = plt.subplots(figsize=(10, 6))
-    # Scatter plot
-    ax.scatter(feature, target)
-    # Add labels and title
-    ax.set_xlabel(labelx)
-    ax.set_ylabel(labely)
-    ax.set_title(title)
-    # Add grid
-    ax.grid(True)
-    st.pyplot(fig)
 
 def train_model(X_train_scaled, y_train):
     clf = st.session_state.clf 
