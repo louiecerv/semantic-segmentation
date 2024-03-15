b24bee81e3e0dc14b41ec0a7ed7415cba8f2bbc8
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-16 07:00:51 +0800

    Save

--- pages/1The_Dataset.py
+++ pages/1The_Dataset.py
@@ -15,13 +15,21 @@ def app():
     text = """Replace with description of CIFAR 10"""
     st.write(text)
 
+    progress_bar = st.progress(0, text="Loading the images, please wait...")
+
     # Load the CIFAR-10 dataset
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 
-    # Define class names
-    #class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-    #            'dog', 'frog', 'horse', 'ship', 'truck']
+    # update the progress bar
+    for i in range(100):
+        # Update progress bar value
+        progress_bar.progress(i + 1)
+        # Simulate some time-consuming task (e.g., sleep)
+        time.sleep(0.01)
+    # Progress bar reaches 100% after the loop completes
+    st.success("Image dataset loading completed!") 
+
 
     # Create the figure and a grid of subplots
     fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(6, 8))
@@ -36,24 +44,22 @@ def app():
         # Display the image
         ax.imshow(train_images[500 + i], cmap=plt.cm.binary)
         # Add the image label
-        #ax.set_xlabel(feature_names[train_labels[i][0]])
         ax.set_xlabel(train_labels[i][0])
 
     # Show the plot
     plt.tight_layout()  # Adjust spacing between subplots
     st.pyplot(fig)
 
-
     # Normalize pixel values to be between 0 and 1
     train_images, test_images = train_images / 255.0, test_images / 255.0
 
    # Define MLP parameters    
-    st.sidebar.subheader('Set the MLP Parameters')
+    st.sidebar.subheader('Set the CNN Parameters')
     options = ["relu", "tanh", "logistic"]
     activation = st.sidebar.selectbox('Select the activation function:', options)
 
     options = ["adam", "lbfgs", "sgd"]
-    solver = st.sidebar.selectbox('Select the solver:', options)
+    optimizer = st.sidebar.selectbox('Select the optimizer:', options)
 
     hidden_layers = st.sidebar.slider(      
         label="How many hidden layers? :",
@@ -79,24 +85,22 @@ def app():
 
     # Define the CNN architecture
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.Conv2D(32, (3, 3), activation=activation, input_shape=(32, 32, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation=activation))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation=activation))
     model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(64, activation=activation))
     model.add(layers.Dense(10))
 
     # Compile the model
-    model.compile(optimizer='adam',
+    model.compile(optimizer=optimizer,
                 loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                 metrics=['accuracy'])
 
     #store the clf object for later use
     st.session_state.model = model
-
-
 
     if st.button('Start Training'):
         progress_bar = st.progress(0, text="Training the MLP regressor can take up to five minutes please wait...")
@@ -113,6 +117,7 @@ def app():
         st.success("Regressor training completed!") 
         st.write("Use the sidebar to open the Performance page.")
 
+@st.cache_resource
 def train_model(model):
     # Train the model
     history = model.fit(train_images, train_labels, epochs=10, 
