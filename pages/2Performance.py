b2eb0e92d5c0b5c7d3cd83ac1c62665e1615aa3f
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-19 19:05:13 +0000

    SAVE

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -32,10 +32,10 @@ def present_image(imagefile):
 
     # Get the class with the highest probability
     predicted_class = tf.math.argmax(result, axis=1)  # Argmax along axis=1 for class index
-
+    st.write(predicted_class)
     # Get the actual integer index (assuming the first element in result)
     predicted_class_index = int(predicted_class.numpy()[0])
-    st.write('Object classes found in the CIFAR-10 Dataset:')
+    st.write(predicted_class_index)
     st.subheader('Predicted class:' + class_names[predicted_class_index])
 
  
