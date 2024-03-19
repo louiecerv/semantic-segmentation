09c915ee0835c09400c7137f39378dc036170d64
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-19 19:12:52 +0000

    Save

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -36,7 +36,7 @@ def present_image(imagefile):
     # Get the actual integer index (assuming the first element in result)
     predicted_class_index = int(predicted_class.numpy()[0])
     st.write(predicted_class_index)
-    st.write('Predicted class:' + class_names[predicted_class_index])
+    st.write('Predicted class:' + str(class_names[predicted_class_index]))
 
  
 #run the app
